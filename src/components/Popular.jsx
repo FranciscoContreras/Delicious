3d68a6cb775d30f07dcf4cd6de427cb424f1f75c
FranciscoContreras <Contrerasfrancisco2010@gmail.com>
2022-11-13 14:03:30 -0800

    Data pull commit

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,7 +1,21 @@
+import { useEffect } from "react";
+
 function Popular() {
-  return (
-    <div>Popular</div>
-  )
+
+  useEffect(() => {
+    getPopular(); // Import the populr category the api and activate
+  },[]); // Empty array to pass possible aditional information
+
+  // import data
+  const getPopular = async () => {
+    const api   = 
+                await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`);
+    const data  = 
+                await api.json();
+  }
+
+
+  return <div>Popular</div>
 }
 
 export default Popular
