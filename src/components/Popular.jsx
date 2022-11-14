f673e69ee01a6afefa51561a8beb67f7e65fd0f9
FranciscoContreras <Contrerasfrancisco2010@gmail.com>
2022-11-13 21:03:14 -0800

    Updated styles for the content cards

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { useEffect, useState } from "react";
 import styled from "styled-components";
+import { Splide, SplideSlide } from '@splidejs/react-splide';
+import '@splidejs/splide/dist/css/splide.min.css';
+
 
 function Popular() {
 
@@ -19,15 +22,47 @@ function Popular() {
 
   return (
     <div>
-      {popular.map((recipe) => {
-        return (
-          <div key={recipe.id}>
-            <p>{recipe.title}</p>
-          </div>
-        );
-      })}
+      <Wrapper>
+        <h3>Popular Picks</h3>
+        
+        <Splide 
+          options={{
+            perPage: 4,
+            arrows: false,
+            pagination: false,
+            drag: 'free',
+            gap: '5rem'
+          }}
+        >
+          {popular.map((recipe) => {
+            return(
+              <SplideSlide>
+                <Card>
+                  <p>{recipe.title}</p>
+                  <img src={recipe.image} alt={recipe.title} />
+                  {/* <p>{recipe.summary}</p> */}
+                </Card>
+              </SplideSlide>
+            );
+          })};
+        </Splide>
+      </Wrapper>
     </div>
   );
 }
+
+const Wrapper = styled.div`
+  margin: 4rem 0rem; 
+`;
+
+const Card = styled.div`
+  min-height: 25rem;
+  border-radius: 2rem;
+  overflow: hidden;
+
+  img{
+    border-radius: 2rem;
+  }
+`;
 
 export default Popular
